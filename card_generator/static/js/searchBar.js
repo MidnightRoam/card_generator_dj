9845d5a2648cbbc354c69530340e2b99016eec59
Uladzislau Saldatsenka <midnightroam1@gmail.com>
2022-12-19 15:46:23 +0100

    Search bar javascript component for finding user card by any data was developed

---
+++ card_generator/static/js/searchBar.js
@@ -0,0 +1,23 @@
+function search() {
+    window.onload = () => {
+        let input = document.querySelector('#search-field');
+        input.oninput = function() {
+            let value = this.value.trim(); // Catch the entered letters in the input
+            let list = document.querySelectorAll('.card');
+
+            if(value) {
+                list.forEach(elem => {
+                   if (elem.innerText.search(value) === -1) {
+                       elem.classList.add('hide');
+                   }
+                });
+            } else {
+                list.forEach(elem => {
+                    elem.classList.remove('hide')
+                });
+            }
+        };
+    };
+}
+
+search();
